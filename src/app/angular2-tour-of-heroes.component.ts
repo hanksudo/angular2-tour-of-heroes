8a6ee854703b72701efda3723e326bd75790b2b3
Hank Wang <drapho@gmail.com>
2016-06-01 17:01:04 +0800

    seperate heroes by component

--- src/app/angular2-tour-of-heroes.component.ts
+++ src/app/angular2-tour-of-heroes.component.ts
@@ -1,30 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero, HeroService } from './shared';
-import { HeroDetailComponent } from './hero-detail';
-
+import { HeroService } from './shared';
+import { HeroesComponent } from './heroes';
 
 @Component({
   moduleId: module.id,
   selector: 'angular2-tour-of-heroes-app',
   templateUrl: 'angular2-tour-of-heroes.component.html',
   styleUrls: ['angular2-tour-of-heroes.component.css'],
-  directives: [HeroDetailComponent],
+  directives: [HeroesComponent],
   providers: [HeroService]
 })
 export class Angular2TourOfHeroesAppComponent implements OnInit {
   title = 'One Piece';
-  heroes: Hero[];
-  selectedHero: Hero;
   
-  constructor(private heroService: HeroService) {}
-  
-  getHeroes() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
+  constructor() {}
   
   ngOnInit() {
-    this.getHeroes();
+    
   }
-  
-  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero, HeroService } from '../shared';
+import { HeroDetailComponent } from '../hero-detail';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css'],
+  directives: [HeroDetailComponent]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) {}
+  
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+}
