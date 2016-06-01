2e6f682b31215c8fcabadb71adb622a88b49479c
Hank Wang <drapho@gmail.com>
2016-06-01 16:33:27 +0800

    styling and master/detail

--- src/app/angular2-tour-of-heroes.component.ts
+++ src/app/angular2-tour-of-heroes.component.ts
@@ -13,8 +13,20 @@ export class Hero {
 })
 export class Angular2TourOfHeroesAppComponent {
   title = 'One Piece';
-  hero: Hero = {
-    id: 1,
-    name: 'Monkey D.Luffy' 
-  }
+  heroes = HEROES;
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+var HEROES: Hero[] = [
+  { "id": 1, "name": "Monkey D.Luffy" },
+  { "id": 2, "name": "Roronoa Zoro" },
+  { "id": 3, "name": "Nami" },
+  { "id": 4, "name": "Usopp" },
+  { "id": 5, "name": "Vinsmoke Sanji" },
+  { "id": 6, "name": "Tony Tony Chopper" },
+  { "id": 7, "name": "Nico Robin" },
+  { "id": 8, "name": "Franky" },
+  { "id": 9, "name": "Brook" }
+];
